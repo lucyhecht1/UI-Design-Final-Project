056a5c786337056b42dd7b4c4964beaa037deabd
lucyhecht1 <lbh2148@columbia.edu>
2024-04-17 11:16:56 -0400

    Next button in quiz

--- Sliding-Puzzle-main/static/quiz.js
+++ Sliding-Puzzle-main/static/quiz.js
@@ -2,7 +2,21 @@ $(document).ready(function () {
   document.getElementById("startBtn").addEventListener("click", function () {
     startTimer();
     // Additional puzzle logic here
+
   });
 
   // Example puzzle completion logic (adjust as needed)
+
+  function clickNextBut(){
+    console.log("got her");
+    // Click event handler for the next button
+    let currentId = window.location.pathname.split('/').pop();
+    let nextId = parseInt(currentId) + 1;
+    nextId = nextId <= 3 ? nextId : 3;
+    $('#nextButton').click(function() {
+        window.location.href = '/quiz/' + nextId;
+    }
+    )}
+      
+    clickNextBut();
 });
